1c94cf0f8254cc124ee444f81bcda3c8440988f5
DenisRaymer <to.mars.66.miles@gmail.com>
2021-08-30 13:21:11 +0300

    fea: Added sass module and layout "MainLayout"

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css';
+import React from 'react';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body { font-family: 'Montserrat', sans-serif; }
+      `}</style>
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
