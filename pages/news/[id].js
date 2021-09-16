847a13bbd6da3a5d93030c102f8afc7db66d8864
DenisRaymer <to.mars.66.miles@gmail.com>
2021-09-16 03:06:33 +0300

    fea: Added news pages

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,19 +1,32 @@
 import styles from '../../styles/Home.module.scss';
+import Link from 'next/link';
 import { MainLayout } from '../../layouts';
 
-export default function Post() {
+export default function Post({
+  news
+}) {
   return (
     <MainLayout pageTitle="Name news">
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Post <a href="https://nextjs.org">Next.js!</a>
+          <Link href={`/news`}>
+            <a>&larr;</a>
+          </Link>
+          {news.title}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {news.content}
+          <code className={styles.code}>pages/{news.id}</code>
         </p>
       </main>
     </MainLayout>
   );
 }
+
+Post.getInitialProps = async ({ query }) => {
+  const response = await fetch(`http://localhost:3200/news/${query.id}`);
+  const news = await response.json();
+
+  return { news };
+};
--- pages/news/index.js
+++ pages/news/index.js
@@ -2,25 +2,30 @@ import styles from '../../styles/Home.module.scss';
 import Link from 'next/link';
 import { MainLayout } from '../../layouts';
 
-export default function News() {
+export default function News({
+  news
+}) {
   return (
     <MainLayout pageTitle="All news">
       <main className={styles.main}>
         <div className={styles.grid}>
-          <Link href={'/news/qu346'}>
-            <a className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-          </Link>
-          <Link href={'/news/21653'}>
-            <a className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-          </Link>
+          {news.map(news => (
+            <Link href={`/news/${news.id}`}>
+              <a className={styles.card}>
+                <h2>{news.title} &rarr;</h2>
+                <p>{news.description}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </MainLayout>
   );
 }
+
+News.getInitialProps = async () => {
+  const response = await fetch('http://localhost:3200/news');
+  const news = await response.json();
+
+  return { news };
+};
